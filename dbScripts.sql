52b8cd034a9a1a684bc749c9c8d2e4fb15b4e5ee
Don Mahathanthila <58053100+dampiya88@users.noreply.github.com>
2024-12-25 14:30:49 -0800

    db scripts

---
+++ dbScripts.sql
@@ -0,0 +1,24 @@
+-- Create user table (configured auto incrementing id , assumed that the database supports JSON data type
+--  user data is stored in JSON format in the userInfo column  (could be normalized to separate tables if needed)
+CREATE TABLE User (
+    id INT PRIMARY KEY,
+    userInfo JSON
+);
+
+-- SQL Script sort the user data by name (assuming the name key is stored in every row of userInfo column)
+SELECT userInfo
+FROM user
+ORDER BY JSON_EXTRACT(userInfo, '$.name');
+
+--SQL Script to update database
+UPDATE user
+SET userInfo = jsonb_set(userInfo, '{email}', '"coding@arrowheadcu.org"', false)
+WHERE id = 9;
+
+-- SQL Script to filter users by longitude (assuming the lng key is stored in every row of userInfo column)
+SELECT *
+FROM user
+WHERE JSON_EXTRACT(userInfo, '$.address.geo.lng') > -110.455;
+
+
+
